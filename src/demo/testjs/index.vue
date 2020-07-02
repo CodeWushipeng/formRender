6a77b70d5383f4e53eae5739e58d8bd95817471f
fengwenxin <fengwenxin@gcstgroup.com>
2020-07-02 16:13:31 +0800

    测试demo3

--- src/demo/testjs/index.vue
+++ src/demo/testjs/index.vue
@@ -9,29 +9,29 @@
 
 <script>
 
-    import {utliTools,evalFunc,user,platform,func} from './ls/utils-fn';
-    import {nodes,inputConfig_,inputConfig,result} from './ls/inputConfig'
-
-    console.log('utliTools',utliTools);
+    // import {utliTools,evalFunc,user,platform,func} from './ls/utils-fn';
+    // import {nodes,inputConfig_,inputConfig,result} from './ls/inputConfig'
+    //
+    // console.log('utliTools',utliTools);
     export default {
         name: "index.js",
         data(){
             return {
-                util: utliTools,
+                // util: utliTools,
                 transObj:null
             }
         },
         created(){
-            console.log('nodes',{...nodes});
+            // console.log('nodes',{...nodes});
 
             try {
                 // 1
-                const testCode = eval("(" + result + ")");
+                // const testCode = eval("(" + result + ")");
                 // const testCode = eval("(" + inputConfig_ + ")");
                 // console.log('typeof execute', testCode);
                 // console.log('typeof execute',typeof testCode);
 
-                console.log('excute result:', testCode(user,platform,nodes,this.util));
+                // console.log('excute result:', testCode(user,platform,nodes,this.util));
 
             }catch (error) {
                 // console.log('inputConfig解析出错',inputConfig)
