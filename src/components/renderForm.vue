28cbe68b999a62fa3bea902f1c8fb6b09157563c
wushipeng <18234097765@163.com>
2020-07-15 08:52:38 +0800

    renderForm组件动态数据流控数据处理添加grid类型处理

--- src/components/renderForm.vue
+++ src/components/renderForm.vue
@@ -2,11 +2,16 @@
   <div class="form-wrap" ref="renderForm">
     <!--jsonData: {{jsonData}}-->
     <!--<hr>-->
-    list: {{configdata.list}}
+    list: {{ configdata.list }}
     <!--formdata: {{formdata}}-->
     <!--tempValue: {{tempValue}}-->
     <!--<hr>-->
-    <fm-generate-form :remote="remoteFuncs" :data="jsonData" :value="formdata" ref="generateForm"></fm-generate-form>
+    <fm-generate-form
+      :remote="remoteFuncs"
+      :data="jsonData"
+      :value="formdata"
+      ref="generateForm"
+    ></fm-generate-form>
   </div>
 </template>
 
@@ -15,26 +20,26 @@ import { Loading } from "element-ui";
 import fmGenerateForm from "./GenerateForm";
 import { getFormList } from "../api/forms";
 import handler from "../components/render";
-import {RES_OK} from "@/api/config";
+import { RES_OK } from "@/api/config";
 export default {
   name: "render-form",
   components: {
-    fmGenerateForm
+    fmGenerateForm,
   },
   props: {
     configdata: {
       type: Object,
-      default: () => {}
+      default: () => {},
     },
     remoteFuncs: {
       type: Object,
-      default: () => {}
+      default: () => {},
     },
     url: {
       // 查询表单的服务
       type: String,
-      default: () => ""
-    }
+      default: () => "",
+    },
   },
   mixins: [handler],
   data() {
@@ -43,11 +48,11 @@ export default {
       tempValue: {}, //处理流控数据的变量
       formdata: {},
       dyData: {}, //动态数据
-      loadingInstance: null
+      loadingInstance: null,
     };
   },
   created() {
-    console.log('created...',this.configdata);
+    console.log("created...", this.configdata);
     this._inits();
   },
   destroyed() {
@@ -66,16 +71,16 @@ export default {
           inputConfig,
           checkStart,
           nodeCode,
-          nextNode
+          nextNode,
         } = this.configdata.list[0];
         return {
           inputConfig,
           checkStart,
           nodeCode,
-          nextNode
+          nextNode,
         };
       }
-    }
+    },
   },
   methods: {
     showLoading() {
@@ -102,46 +107,48 @@ export default {
       // 3
       if (list instanceof Array && list.length > 0 && list[0]) {
         const { inputFormCode } = list[0];
-        if(!inputFormCode){
-            this.$notify.error({
-                title: "消息",
-                message: "表单名称inputFormCode不能为空"
-            });
-            return;
+        if (!inputFormCode) {
+          this.$notify.error({
+            title: "消息",
+            message: "表单名称inputFormCode不能为空",
+          });
+          return;
         }
         getFormList(this.url, { formCode: inputFormCode })
-            .then(res => {
-              console.log("=====form-making-secondary===res============", res);
-              const { rspCode } = res;
-              if (rspCode == RES_OK) {
-                const rest = res.define;
-                if (rest && rest.records.length > 0) {
-                  const formContent = rest.records[0]["formContent"];
-                  // 设置数据
-                  let temp =
-                    typeof formContent == "string"  ? JSON.parse(formContent) : formContent;
-                  this.flatGridData(temp)
-                  this.handleDynamicData(temp);
-                  this.handleDynamicInFlow(temp);
-                  this.jsonData = temp;
-                  console.log('temp',temp);
-                  console.log('jsonData',this.jsonData);
-                } else {
-                  this.$notify.error({
-                    title: "消息",
-                    message: "没有搜索到数据"
-                  });
-                }
+          .then((res) => {
+            console.log("=====form-making-secondary===res============", res);
+            const { rspCode } = res;
+            if (rspCode == RES_OK) {
+              const rest = res.define;
+              if (rest && rest.records.length > 0) {
+                const formContent = rest.records[0]["formContent"];
+                // 设置数据
+                let temp =
+                  typeof formContent == "string"
+                    ? JSON.parse(formContent)
+                    : formContent;
+                this.flatGridData(temp);
+                this.handleDynamicData(temp);
+                this.handleDynamicInFlow(temp);
+                this.jsonData = temp;
+                console.log("temp", temp);
+                console.log("jsonData", this.jsonData);
               } else {
                 this.$notify.error({
                   title: "消息",
-                  message: "查询失败"
+                  message: "没有搜索到数据",
                 });
               }
-            })
-            .catch(error => {
-              console.log(error);
-            });
+            } else {
+              this.$notify.error({
+                title: "消息",
+                message: "查询失败",
+              });
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+          });
       }
     },
     // 处理流控数据中带有的动态数据
@@ -152,8 +159,18 @@ export default {
       let formLists = temp.list;
       for (let i = 0; i < formLists.length; i++) {
         for (let key in this.tempValue) {
-          if (formLists[i].model == key) {
-            formLists[i].options.defaultValue = this.tempValue[key];
+          if (formLists[i].type == "grid") {
+            formLists[i].columns.forEach((element) => {
+              element.list.forEach((item) => {
+                if (item.model == key) {
+                  item.options.defaultValue = this.tempValue[key];
+                }
+              });
+            });
+          } else {
+            if (formLists[i].model == key) {
+              formLists[i].options.defaultValue = this.tempValue[key];
+            }
           }
         }
       }
@@ -179,11 +196,30 @@ export default {
             console.log(tempJson, temp);
             if (tempJson != "" && tempJson != undefined) {
               this.$set(formLists[i].options, "defaultValue", tempJson);
-              // formLists[i].options.defaultValue = tempJson;
-              // this.models[formLists[i].modeltempJson] = tempJson
               console.log(formLists[i].options.defaultValue);
             }
           }
+        } else if (formLists[i].type == "grid") {
+          formLists[i].columns.forEach((element) => {
+            element.list.forEach((item) => {
+              if (
+                formLists[i].options.defaultValue &&
+                formLists[i].options.defaultValue[0] == "@"
+              ) {
+                var temp = formLists[i].options.defaultValue.substring(1);
+                try {
+                  var tempJson = eval(temp);
+                } catch (error) {
+                  throw new Error(error);
+                }
+                console.log(tempJson, temp);
+                if (tempJson != "" && tempJson != undefined) {
+                  this.$set(formLists[i].options, "defaultValue", tempJson);
+                  console.log(formLists[i].options.defaultValue);
+                }
+              }
+            });
+          });
         }
       }
     },
@@ -197,7 +233,7 @@ export default {
         return false;
       }
       getFormList(this.url, { formCode: outputFromCode })
-        .then(res => {
+        .then((res) => {
           const { rspCode } = res;
           this.hideLoading();
           if (rspCode == RES_OK) {
@@ -208,30 +244,33 @@ export default {
                 typeof formContent == "string"
                   ? JSON.parse(formContent)
                   : formContent;
-              if(outputCofig){
-                  this.tempValue = this._solveConfigJs(outputCofig);
-              }else{
-                  this.tempValue = {}
+              if (outputCofig) {
+                this.tempValue = this._solveConfigJs(outputCofig);
+              } else {
+                this.tempValue = {};
               }
               this.handleDynamicData(temp);
               this.handleDynamicInFlow(temp);
               this.jsonData = temp;
-                outputCofig && console.log("this._solveConfigJs(outputCofig)",this._solveConfigJs(outputCofig)
-              );
+              outputCofig &&
+                console.log(
+                  "this._solveConfigJs(outputCofig)",
+                  this._solveConfigJs(outputCofig)
+                );
             } else {
               this.$notify.error({
                 title: "消息",
-                message: "没有搜索到数据"
+                message: "没有搜索到数据",
               });
             }
           } else {
             this.$notify.error({
               title: "消息",
-              message: "响应页面失败"
+              message: "响应页面失败",
             });
           }
         })
-        .catch(err => {
+        .catch((err) => {
           throw new Error(err);
           this.hideLoading();
         });
@@ -244,7 +283,7 @@ export default {
           platform,
           user,
           nodes,
-          utils
+          utils,
         });
         // console.log('{ configdata} ',JSON.stringify(this.configdata))
         const resCode = `function _execute(user, platform, nodes, utils){  ${JsCode}  return main(...arguments);}`;
@@ -277,16 +316,16 @@ export default {
     // 获取表单数据
     getData() {
       return this.$refs.generateForm.getData();
-    }
+    },
   },
   watch: {
     "configdata.list": {
       deep: true,
       handler(list) {
         this._inits();
-      }
-    }
-  }
+      },
+    },
+  },
 };
 </script>
 
